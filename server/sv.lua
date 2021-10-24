6fa3e659421312bf107c4c4d2363cd184d542d21
BombayV <OMGmsebastian@gmail.com>
2021-10-24 01:24:22 -0400

    feat(server): added framework support

--- server/sv.lua
+++ server/sv.lua
@@ -4,20 +4,42 @@ GlobalState.ActiveLaundering = false
 
 local Framework
 
----Returns if player has money
+---Returns if player has money to change
 ---@param playerId number
+---@param resultMoney number
 ---@return boolean
-local function DoesPlayerHaveMoney(playerId)
+local function DoesPlayerHaveMoney(playerId, resultMoney)
     if state == 'none' then
         -- If you want to implement into your framework you can do it here
-        return true, print('Hey, you have money!')
+        return true
     elseif state == 'esx' then
-        
+        if not Framework then
+            TriggerEvent('esx:getSharedObject', function(obj) Framework = obj end)
+        end
+        if Framework then
+            local xPlayer = Framework.GetPlayerFromId(playerId)
+            if xPlayer then
+                local blackMoney = xPlayer.getAccount('black_money').money
+                if blackMoney > 0 then
+                    if resultMoney < 5000000 then
+                        if (blackMoney - resultMoney) > 0 then
+                            return true
+                        else
+                            return false
+                        end
+                    else
+                        --DropPlayer(playerId, 'sus')
+                        return false, print(tostring(playerId) .. ' tried doing some sus stuff in ' .. GetCurrentResourceName())
+                    end
+                end
+            end
+        end
     elseif state == 'qbcore' then
         
     elseif state == 'vrp' then
 
     end
+    return false
 end
 
 RegisterNetEvent('ev:launderData', function(data)
@@ -28,8 +50,6 @@ RegisterNetEvent('ev:launderData', function(data)
         return false, print(tostring(playerId) .. ' tried doing some sus stuff in ' .. GetCurrentResourceName())
     end
 
-    
-
     if data then
         local input = data.cantiInput
         if not type(data.cantiInput) == "string" then
@@ -37,8 +57,16 @@ RegisterNetEvent('ev:launderData', function(data)
             return false, print(tostring(playerId) .. ' tried doing some sus stuff in ' .. GetCurrentResourceName())
         end
         if math.floor((tonumber(input) / 100) * data.porcentaje) == data.cantiPorcentaje then
-            if DoesPlayerHaveMoney(playerId) then
+            if DoesPlayerHaveMoney(playerId, tonumber(input)) then
+                if state == 'none' then
+                    print('Hey, you have money!')
+                elseif state == 'esx' then
+
+                elseif state == 'qbcore' then
         
+                elseif state == 'vrp' then
+            
+                end
             end
         else
             --DropPlayer(playerId, 'sus')
