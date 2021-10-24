4496d61f2ae38635ac57bf8f94d0f4c2b09bc112
fishiidev <85807354+fishiidev@users.noreply.github.com>
2021-10-23 21:21:52 -0600

    feat(ui/js): added data listeners uwu 7u7

--- ui/js/script.js
+++ ui/js/script.js
@@ -1,5 +1,18 @@
 window.addEventListener('load', () => {
     this.addEventListener('message', e => {
-        
+        switch (e.data.action) {
+            case "openMenu":
+
+                doc.getElementById("contrato").style.display = "flex";
+
+            break
+        }
     })
+    doc.addEventListener("onkeyup", e => {
+        if (e.key == "Escape") {
+            doc.getElementById("contrato").style.display = "none";
+            fetchNUI("getMoneyData")
+        }
+    });
+
 })
